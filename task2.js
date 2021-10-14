c963971abad83a620e61dcf670f56f1f396ecbb9
manchikooo <manchiko.man31@mail.ru>
2021-10-14 18:51:31 +0300

    js task 2

---
+++ task2.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8
+  } else {
+    return number * 9
+  }
+}
