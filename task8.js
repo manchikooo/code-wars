62c69f20eec54d60d04711393d280449cd7a99cd
manchikooo <manchiko.man31@mail.ru>
2021-10-15 19:55:02 +0300

    js task 8

---
+++ task8.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+function even_or_odd(number) {
+  let even = 'Even'
+  let odd = 'Odd'
+  if (number % 2 == 0) {
+    return even
+  } else return odd
+}
