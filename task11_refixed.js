14e35c694e9e8287c59568897785bc62b1a01fd9
manchikooo <manchiko.man31@mail.ru>
2021-10-16 10:41:40 +0300

    js task 11 refixed

--- task11_refixed.js
+++ task11_refixed.js
@@ -1,13 +1,18 @@
 // https://www.codewars.com/kata/5a360620f28b82a711000047/train/javascript/6169b53d98fd9f001d6a1275
 
 function defineSuit(card) {
-  if (card.slice(-1) == '♣') {
-    return 'clubs'
-  } else if (card.slice(-1) == '♦') {
-    return 'diamonds'
-  } else if (card.slice(-1) == '♥') {
-    return 'hearts'
-  } else {
-    return 'spades'
+  switch (card.slice(-1)) {
+    case '♣':
+      return 'clubs'
+      break
+    case '♦':
+      return 'diamonds'
+      break
+    case '♥':
+      return 'hearts'
+      break
+    case '♠':
+      return 'spades'
+      break
   }
 }
