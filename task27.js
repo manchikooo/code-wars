19c619ba436ff5128d4afbb1e799b09f88ed9e48
manchikooo <manchiko.man31@mail.ru>
2021-10-17 16:45:09 +0300

    js task 27

---
+++ task27.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+
+function apple(x) {
+  if (x ** 2 > 1000) {
+    return "It's hotter than the sun!!"
+  }
+  return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
