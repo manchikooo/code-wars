d6ac8eb9d059b09327452a9d060907b05e1e877c
manchikooo <manchiko.man31@mail.ru>
2021-10-17 18:41:40 +0300

    js task 31

---
+++ task31.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0) == 'r') {
+      return name + " plays banjo" 
+    }
+    if (name.charAt(0) == 'R') {
+      return name + " plays banjo" 
+    }
+    else return name + " does not play banjo"
+  
+  }
