65bea474986f14229beeac4e6f794910f1f61830
manchikooo <manchiko.man31@mail.ru>
2021-10-26 04:54:44 +0300

    task 32

--- task11_refixed.js
+++ task11_refixed.js
@@ -1,18 +1,14 @@
 // https://www.codewars.com/kata/5a360620f28b82a711000047/train/javascript/6169b53d98fd9f001d6a1275
 
 function defineSuit(card) {
-  switch (card.slice(-1)) {
-    case '♣':
-      return 'clubs'
-      break
-    case '♦':
-      return 'diamonds'
-      break
-    case '♥':
-      return 'hearts'
-      break
-    case '♠':
-      return 'spades'
-      break
-  }
+    switch (card.slice(-1)) {
+        case '♣':
+            return 'clubs'
+        case '♦':
+            return 'diamonds'
+        case '♥':
+            return 'hearts'
+        case '♠':
+            return 'spades'
+    }
 }
---
+++ task32.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/58649884a1659ed6cb000072/train/javascript
+
+function updateLight(current) {
+    if (current == "green") {
+        return "yellow"
+    }
+    else if (current == "yellow"){
+        return "red"
+    }
+    else if (current == "red"){
+        return "green"
+    }
+}
