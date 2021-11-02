df9188489697104de8e4c248595854b9311e588a
manchikooo <manchiko.man31@mail.ru>
2021-11-02 03:38:50 +0300

    js task 43

---
+++ task43.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+var min = function (list) {
+    let minnum = Math.min(...list)
+    return minnum
+}
+
+var max = function (list) {
+    let maxnum = Math.max(...list)
+    return maxnum
+}
