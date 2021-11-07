af8b2d8196b95eb24f121fa3b2340ae2afaddc95
manchikooo <manchiko.man31@mail.ru>
2021-11-08 02:31:14 +0300

    js task 47

--- task47.js
+++ task47.js
@@ -1,11 +1,7 @@
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
 var number = function (array) {
-    if (array.length == 0) {
-        return []
-    }
-    for (let i = 0; i < array.length; i++) {
-        let a = array.map((v, i) => `${i + 1}: ${v}`)
-        return a
-    }
+    let c = array.map((a, b) =>
+        `${b + 1}: ${a}`)
+    return c
 }
